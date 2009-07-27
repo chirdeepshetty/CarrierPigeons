1020a1ba5005899894104da0903a7b7eef26264d
unknown <ashishs@.corporate.thoughtworks.com>
2009-07-27 16:29:10 +0530

    Added Tests.

---
+++ DomainModel/DomainModel/Tests/UserTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DomainModel;
+using NUnit.Framework;
+
+
+
+namespace DomainModel.Tests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        public void TestUserConstructor()
+        {
+            Email email = new Email("user@carrierpigeons.com");
+            UserName name = new UserName();
+            User user = new User(email, name);
+        }
+    }
+
+}
