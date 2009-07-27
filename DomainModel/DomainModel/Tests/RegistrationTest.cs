2cb842a37900b3b789264d4c61dd437ed05c5546
unknown <venkatn@.corporate.thoughtworks.com>
2009-07-27 18:41:31 +0530

    Venky - Added Registration Service

--- DomainModel/DomainModel/Tests/RegistrationTest.cs
+++ DomainModel/DomainModel/Tests/RegistrationTest.cs
@@ -1,19 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using DomainModel.UserRegistration;
+using NHibernate;
 using NUnit.Framework;
 
 namespace DomainModel.Tests
 {
     [TestFixture]
-    public class RegistrationTest
+    public class RegistrationTest : InMemoryTestFixtureBase
     {
+        private ISession session;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            InitalizeSessionFactory(new FileInfo("src/User.hbm.xml"));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            session = this.CreateSession();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            session.Dispose();
+        }
+
+
         [Test]
         public void TestCreateUser()
         {
-          
+            UserName name =new UserName("first","last");
+            var user = new User(new Email("test@test.com"), name);
+            session.Save(user);
+
         }
     }
 }
