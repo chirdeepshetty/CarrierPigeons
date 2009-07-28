6789a193a88fbc850bf6777a91c0a30decb69765
unknown <ashishs@.corporate.thoughtworks.com>
2009-07-28 17:06:55 +0530

    added validationbs to request view

--- DomainModel/Website/Website/Models/CreateRequestResponse.cs
+++ DomainModel/Website/Website/Models/CreateRequestResponse.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
+
+using System.Web.Mvc;
+using DomainModel;
 
 namespace Website.Models
 {
-    public class CreateRequestResponse
+    public class CreateRequestResponse : IDataErrorInfo
     {
         public string PackageDescription { get; set; }
 
@@ -20,5 +25,38 @@ namespace Website.Models
         public string DestinationPlace { get; set; }
 
         public string DestinationDate { get; set; }
+
+        #region IDataErrorInfo Members
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string propName]
+        {
+            get
+            {
+                DateTime datetime;
+                if ((propName == "PackageDescription") && string.IsNullOrEmpty(PackageDescription))
+                    return "Please enter a description for your package.";
+                if ((propName == "PackageDimensions") && string.IsNullOrEmpty(PackageDimensions))
+                    return "Please enter the dimensions for your package.";
+                if ((propName == "PackageWeight") && string.IsNullOrEmpty(PackageWeight))
+                    return "Please enter the weight of your package.";
+                if ((propName == "OriginPlace") && string.IsNullOrEmpty(OriginPlace))
+                    return "Please enter the origin station.";
+                if ((propName == "OriginDate") && !DateTime.TryParse(OriginDate, out datetime))
+                    return "Please enter a valid Date.";
+                if ((propName == "DestinationDate") && !DateTime.TryParse(DestinationDate, out datetime))
+                    return "Please enter a valid Date.";
+                if ((propName == "DestinationPlace") && string.IsNullOrEmpty(DestinationPlace))
+                    return "Please enter the destination station.";
+
+                return null;
+            }
+        }
+
+        #endregion
     }
 }
