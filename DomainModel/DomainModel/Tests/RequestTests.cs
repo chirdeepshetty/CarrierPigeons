bec9eda770a50a70bf649df2518b4984fd2d0041
unknown <ashishs@.corporate.thoughtworks.com>
2009-07-28 14:38:44 +0530

    fixed project

--- DomainModel/DomainModel/Tests/RequestTests.cs
+++ DomainModel/DomainModel/Tests/RequestTests.cs
@@ -17,11 +17,7 @@ namespace DomainModel.Tests
             Location fromLocation = new Location("place", travelDate);
             Location toLocation = new Location("place", travelDate);
             Package package = new Package("My Package", "1 Kg", "1x2x3 kg");
-<<<<<<< HEAD:DomainModel/DomainModel/Tests/RequestTests.cs
-            User user = new User(new Email("user@carrierpigeins.com"), new UserName("First", "Last"));
-=======
-            User user = new User(new Email("user@carrierpigeins.com"), new UserName("First","Last"));
->>>>>>> 7565b92096f3ced939ba3f1e48d3f00d825595f3:DomainModel/DomainModel/Tests/RequestTests.cs
+            User user = new User(new Email("user@carrierpigeons.com"), new UserName("First", "Last"));
             Request request = new Request(user, package, fromLocation, toLocation);
 
         }
