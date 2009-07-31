6d49464c84f26f105e14ef3befccead64789e9cd
unknown <ashwinr@.corporate.thoughtworks.com>
2009-07-31 11:40:19 +0530

    added journey and request match controller and pages

--- DomainModel/Tests/RequestTests.cs
+++ DomainModel/Tests/RequestTests.cs
@@ -65,7 +65,6 @@ namespace DomainModel.Tests
             Location toLocation = new Location("place", travelDate);
             Package package = new Package("My Package", "1 Kg", "1x2x3 kg");
             User user = new User(new Email("user@carrierpigeons.com"), new UserName("First", "Last"),"pwd");
-
             Request request = new Request(user, package, fromLocation, toLocation);
 
             IRequestRepository repository = RequestRepository.Instance;
---
+++ Website/Website/Views/MatchList.aspx
@@ -0,0 +1,16 @@
+<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	MatchList
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>List Of Matches for the Request</h2>
+
+    <div>
+       
+    
+    </div>
+
+</asp:Content>
