3486ee05fb71cc1def1fb83cbc6b6782be0397d0
unknown <ashwinr@.corporate.thoughtworks.com>
2009-07-31 12:36:15 +0530

    implemented jouney search by user

--- DomainModel/Tests/JourneyTests.cs
+++ DomainModel/Tests/JourneyTests.cs
@@ -38,5 +38,26 @@ namespace DomainModel.Tests
             Assert.AreEqual("asd@dsf.com", journey.Traveller.Email.EmailAddress);
             
         }
+
+        [Test]
+        public void TestGetJourneyByUser ()
+        {
+            User traveller = new User(new Email("asd@dsf.com"), new UserName("first", "last"), "pwd");
+            RepositoryFactory.GetUserRepository().SaveUser(traveller);
+            Location origin = new Location("London", new TravelDate(DateTime.Now));
+            Location destination = new Location("Mumbai", new TravelDate(DateTime.Now));
+            Journey journey = new Journey(traveller, origin, destination);
+            IJourneyRepository journeyRepository = JourneyRepository.Instance;
+            journeyRepository.Save(journey);
+
+
+            User user = new User(new Email("asd@dsf.com"), new UserName(null, null), null);
+            List<Journey> journeyList = journeyRepository.FindJourneysByUser(user.Email.EmailAddress);
+        }
+
+
+
+
+
     }
-}
+    }
